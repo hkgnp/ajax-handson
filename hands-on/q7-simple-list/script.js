3f97b7e56e86f0ce03e3fd8756f15e76497c6158
hkgnp <ben@pngs.cc>
2021-01-29 17:12:22 +0800

    updated q7b

--- hands-on/q7-simple-list/script.js
+++ hands-on/q7-simple-list/script.js
@@ -3,14 +3,16 @@ document.querySelector('#load-btn').addEventListener('click', () => {
     let response1 = await axios.get('https://anapioficeandfire.com/api/books/');
     let books = response1.data;
 
-    for (let b of books) {
-      async function getPOV(b) {
-        for (let eachPOV of b.povCharacters) {
-          let response2 = await axios.get(eachPOV);
-          let povData = response2.data.name;
-          console.log(povData);
-          document.querySelector('#ul').innerHTML = povData;
-        }
+    for (let j = 0; j < books.length; j++) {
+      let povApiArray = books[j].povCharacters;
+      let povNamesArray = [];
+
+      for (let i = 0; i < povApiArray.length; i++) {
+        let povAPI = povApiArray[i];
+        let response2 = await axios.get(povAPI);
+        console.log(response2.data.name);
+
+        povNamesArray.push(response2.data.name);
       }
 
       // function test() {
@@ -20,10 +22,10 @@ document.querySelector('#load-btn').addEventListener('click', () => {
 
       document.querySelector('#content').innerHTML += `
       <p>
-        <li>Title: ${b.name}</li>
-        <li>Number of Pages: ${b.numberOfPages}</li>
+        <li>Title: ${books[j].name}</li>
+        <li>Number of Pages: ${books[j].numberOfPages}</li>
         <li>POV Characters:</li>
-        <ul id="ul">${getPOV(b)}</ul>
+        <ul>${povNamesArray}</ul>
       </p>
       `;
     }
