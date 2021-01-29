cfee86548e40107a40e09fa1741bb9d18a963007
hkgnp <benpng@u.nus.edu>
2021-01-30 07:27:14 +0800

    Updated q8

--- hands-on/q8-random-users/script.js
+++ hands-on/q8-random-users/script.js
@@ -1,1 +1,20 @@
-let url = "https://randomuser.me/api/?results=10"
+let url = 'https://randomuser.me/api/?results=10';
+
+document.querySelector('#load-btn').addEventListener('click', () => {
+  (async () => {
+    let response = await axios.get(url);
+    let randomUsers = response.data.results;
+    console.log(randomUsers);
+
+    for (let u of randomUsers) {
+      document.querySelector('#users').innerHTML += `
+      <p style = 'border: 1px solid black'>
+      <li><b>First Name:</b> ${u.name.first}</li>
+      <li><b>Last Name:</b> ${u.name.last}</li>
+      <li><b>City:</b> ${u.location.city}</li>
+      <li><b>Photo:</b> <img src=${u.picture.thumbnail} alt='thumbnail'></li>
+      </p>
+      `;
+    }
+  })();
+});
